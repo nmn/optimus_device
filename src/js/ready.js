cce3f4b958ccebb41a2434b1eb9fed85d8c58fd1
Rob Graeber <bgraeber@gmail.com>
2014-06-21 17:46:49 -0700

    Tweaked testing speech

--- src/js/ready.js
+++ src/js/ready.js
@@ -16,18 +16,17 @@ var captureError = function(error) {
 
 module.exports = function(){
   console.log('TEST:',$('.voice')[0]);
-  // var mediaRec = new Media('recording.wav', captureSuccess, captureError);
-  
+  var mediaRec = new Media('recording.wav', captureSuccess, captureError);
   $('.voice').on('touchstart', function(){
-    // mediaRec.startRecord();
+    mediaRec.startRecord();
   });
   $('.voice').on('touchend', function(){
-    // mediaRec.stopRecord();
+    mediaRec.stopRecord();
     // mediaRec.play();
-    speechManager.speakText("I don't know who you are. I don't know what you want. If you are looking for ransom, I can tell you I don't have money. But what I do have are a very particular set of skills, skills I have acquired over a very long career. Skills that make me a nightmare for people like you. If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.");
-    // audioManager.uploadWav('recording.wav', function(speechCmd){
-    //   apiManager.executeCommand(speechCmd);
-    // });
+    speechManager.speakText("I'm on it!");
+    audioManager.uploadWav('recording.wav', function(speechCmd){
+      apiManager.executeCommand(speechCmd);
+    });
   });
 };
 
