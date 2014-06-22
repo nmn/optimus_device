89d094657b61615c628b286875b130cda3cb83af
Naman Goel <naman34@gmail.com>
2014-06-22 02:13:02 -0700

    added ajax upload to wit ai

--- src/js/audioManager.js
+++ src/js/audioManager.js
@@ -1,31 +1,89 @@
-exports.uploadWav = function(audioURI, cb) {
-    var options = new FileUploadOptions();
-    options.fileKey = "file";
-    options.fileName = audioURI.substr(audioURI.lastIndexOf('/')+1);
-    options.mimeType = "audio/wav";
+var reqwest = require('reqwest');
+var $ = require('jquery');
 
-    var params = {};
-    params.value1 = "test";
-    params.value2 = "param";
+module.exports = function(audioBuffer) {
 
-    options.params = params;
+  // reqwest({
+  //   url: 'https://api.wit.ai/speech',
+  //   method: 'post',
+  //   contentType: 'audio/wav',
+  //   headers: {
+  //     'Authorization': 'Bearer DOOZE6JRCO6KG5HUOKSN4M5RDPHOX465',
+  //     'Accept': 'application/vnd.wit.20140528+json'
+  //   },
+  //   data: audioBuffer
+  // })
+  // .then(console.log.bind(console, 'from wit'))
+  // .fail(console.log.bind(console, 'wit fail: '));
 
-    console.log("URI:", options.fileName);
+  return (new Promise(function(resolve, reject){
+    $.ajax({
+      url: "https://api.wit.ai/speech",
+      type: "POST",
+      contentType: 'audio/wav',
+      headers: {
+        'Authorization': 'Bearer DOOZE6JRCO6KG5HUOKSN4M5RDPHOX465',
+        'Accept': 'application/vnd.wit.20140528+json'
+      },
+      dataType: 'audio/wav',
+      data: audioBuffer,
+      processData: false,
+      success: function(res){
+        if(!!res.responseText){
+          resolve(res.responseText);
+        } else {
+          reject(res);
+        }
+      },
+      error: function(res){
+        if(!!res.responseText){
+          resolve(res.responseText);
+        } else {
+          reject(res);
+        }
+      }
+    });
+  }))
+  .then(function(obj){
+    // add function here...
+    
+    console.log(obj);
+  })
 
-    var ft = new FileTransfer();
-    var successCb = function(r) {
-        console.log("Code = " + r.responseCode);
-        console.log("Response = " + r.response);
-        console.log("Sent = " + r.bytesSent);
-        cb("Sample Text");
-    };
-    var failCb = function(error) {
-        alert("An error has occurred: Code = " + error.code);
-        console.log("upload error source " + error.source);
-        console.log("upload error target " + error.target);
-    };
+  
 
-    ft.upload(audioURI, encodeURI("http://some.server.com/upload.php"), successCb, failCb, options);
+  // var options = new FileUploadOptions();
+  // options.fileKey = "file";
+  // options.fileName = audioURI.substr(audioURI.lastIndexOf('/')+1);
+  // options.mimeType = "audio/wav";
+
+  // var params = {};
+  // params.value1 = "test";
+  // params.value2 = "param";
+
+  // options.params = params;
+
+  // console.log("URI:", options.fileName);
+
+  // var ft = new FileTransfer();
+  // var successCb = function(r) {
+  //     console.log("Code = " + r.responseCode);
+  //     console.log("Response = " + r.response);
+  //     console.log("Sent = " + r.bytesSent);
+  //     cb("Sample Text");
+  // };
+  // var failCb = function(error) {
+  //     alert("An error has occurred: Code = " + error.code);
+  //     console.log("upload error source " + error.source);
+  //     console.log("upload error target " + error.target);
+  // };
+
+  // ft.upload(audioURI, encodeURI("http://some.server.com/upload.php"), successCb, failCb, options);
+
+
+  //   curl \
+  // -H 'Authorization: Bearer DOOZE6JRCO6KG5HUOKSN4M5RDPHOX465' \
+  // 'https://api.wit.ai/message?v=20140621&q='
 
    
 };
